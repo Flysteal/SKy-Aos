940f3f366f6e31df91c0eac866554b4d2588a7d9
Your Name <you@example.com>
2025-02-01 12:56:40 +0200

    cmake CMakeDOne

---
+++ build/CMakeFiles/Sky-Aos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sky-Aos.dir/Sky-Aos.cpp.o"
+  "CMakeFiles/Sky-Aos.dir/Sky-Aos.cpp.o.d"
+  "libSky-Aos.a"
+  "libSky-Aos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sky-Aos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Sky-Aos.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Sky-Aos.
+# This may be replaced when dependencies are built.
